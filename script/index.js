b78ad2418fa59336c23c237afe4a6d6294ab9fb3
Skrebnev Maxim <maxim.skrebnev@yandex.ru>
2021-10-04 23:17:06 +0300

    fixFirst

--- script/index.js
+++ script/index.js
@@ -3,23 +3,22 @@ const popupOpen = document.querySelector('.profile__editbutton');
 const popupClose = document.querySelector('.popup__close');
 const nameIput = document.querySelector('.popup__name-input');
 const jobInput = document.querySelector('.popup__job-input');
-const popupButton = document.querySelector('.popup__btn');
 const nameMain = document.querySelector('.profile__title');
 const jobMain = document.querySelector('.profile__subtitle');
 const form = document.querySelector('.popup__form');
 
+nameIput.value = 'Жак-Ив Кусто'; 
+jobInput.value = 'Исследователь океана'; 
+
 function openPopup() {
-  popup.classList.add('active')
+  popup.classList.add('popup__active')
 }
-popupOpen.addEventListener('click', openPopup)
-
 function closePopup() {
-  popup.classList.remove('active')
+  popup.classList.remove('popup__active')
 }
-popupClose.addEventListener('click', closePopup)
 
-nameIput.value = 'Жак-Ив Кусто';
-jobInput.value = 'Исследователь океана';
+popupClose.addEventListener('click', closePopup)
+popupOpen.addEventListener('click', openPopup)
 
 function submitForm(event) {
   event.preventDefault()
