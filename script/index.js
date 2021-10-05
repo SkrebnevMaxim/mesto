5f359dc8eaed921bc0b89b0180ffe4b13be2e82f
Skrebnev Maxim <maxim.skrebnev@yandex.ru>
2021-10-05 21:52:29 +0300

    fixSecondReviu

--- script/index.js
+++ script/index.js
@@ -1,30 +1,31 @@
 const popup = document.querySelector('.popup');
 const popupOpen = document.querySelector('.profile__editbutton');
 const popupClose = document.querySelector('.popup__close');
-const nameIput = document.querySelector('.popup__input_name');
-const jobInput = document.querySelector('.popup__input_job');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
 const nameMain = document.querySelector('.profile__title');
 const jobMain = document.querySelector('.profile__subtitle');
 const form = document.querySelector('.popup__form');
 
 
 function openPopup() {
-  popup.classList.add('popup__active')
-}
-function closePopup() {
-  popup.classList.remove('popup__active')
+  popup.classList.add('popup_active')
+  nameInput.value = nameMain.textContent;
+  jobInput.value = jobMain.textContent; 
 }
 
-popupClose.addEventListener('click', closePopup)
-popupOpen.addEventListener('click', openPopup)
+function closePopup() {
+  popup.classList.remove('popup_active')
+}
 
 function submitForm(event) {
   event.preventDefault()
-
-  nameMain.textContent = nameIput.value;
+  nameMain.textContent = nameInput.value;
   jobMain.textContent = jobInput.value;
 
   closePopup();
 }
- 
 form.addEventListener('submit', submitForm)
+
+popupClose.addEventListener('click', closePopup)
+popupOpen.addEventListener('click', openPopup)
