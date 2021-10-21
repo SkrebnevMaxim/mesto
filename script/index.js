520a39d6ca4a1feeec6eabd22a88abc4753dc3ea
Skrebnev Maxim <maxim.skrebnev@yandex.ru>
2021-10-21 23:49:45 +0300

    сократилФункциюДобавленияПопапов

--- script/index.js
+++ script/index.js
@@ -1,20 +1,22 @@
-const popup = document.querySelector('.popup'); 
-const popupOpen = document.querySelector('.profile__editbutton'); 
-const popupClose = document.querySelector('.popup__close'); 
-const nameInput = document.querySelector('.popup__input_type_name'); 
-const jobInput = document.querySelector('.popup__input_type_job'); 
-const nameMain = document.querySelector('.profile__title'); 
-const jobMain = document.querySelector('.profile__subtitle'); 
-const formEdit = document.querySelector('.popup__form_edit');
-const createPopup = document.querySelector('.createPopup');
+const popupEdit = document.querySelector('.popup__edit-card');
+const popupOpen = document.querySelector('.profile__editbutton');
+const popupClose = document.querySelector('.popup__close');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
+const nameMain = document.querySelector('.profile__title');
+const jobMain = document.querySelector('.profile__subtitle');
+const form = document.querySelector('.popup__form');
+const createPopup = document.querySelector('.popup__create-card');
 const createPopupOpen = document.querySelector('.profile__addbutton');
 const createPopupClose = document.querySelector('.createPopup__close');
 const linkCard = document.querySelector('.createPopup__input_card-link');
 const nameCard = document.querySelector('.createPopup__input_card-name');
-
 const formAdd = document.querySelector('.popup__form_add');
+const cardsElement = document.querySelector('#card');
+const cardTemplate = document.querySelector('.elements').content;
+const elementDelit = document.querySelector('.element__delit');
 
-const initialCards = [
+const initialCards = [ // создаем массив карточек
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -40,38 +42,30 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-const cardsElement = document.querySelector('#card');
-const cardTemplate = document.querySelector('.elements').content;
-const elementDelit = document.querySelector('.element__delit');
+
+
 initialCards.forEach(prependCard)
 
-function createCard(item){
+function createCard(item){ // создаем функцию добавления элементов из массива на страницу
   const element = cardTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__title').innerText = item.name;
+  element.querySelector('.element__title').innerText = item.name; //заполняем карточку элементами
   element.querySelector('.element__photo').src = item.link;
   element.querySelector('.element__like-button').addEventListener('click', function (evt) { //добавляем возможность сделать кнопку like активной(поменять цвет)
     evt.target.classList.toggle('element__like-button_active');
     });
-  element.querySelector('.element__delit').addEventListener('click', function(evt) {  // доюовляем возможность удалять карточки
+  element.querySelector('.element__delit').addEventListener('click', function(evt) {  // добавляем возможность удалять карточки
     evt.target.closest('.element').remove();
   });
 
   return element;
 }
 
-
-function prependCard(item){
+function prependCard(item){ // функция добавления новой карточки
   const element = createCard(item);
   cardsElement.prepend(element)
 }
 
-function popupCreate() { //функция открытия popup создания новых элементов
-  createPopup.classList.add('createPopup_active')
-}
-function popupCreateClose() {  //функция закрытия popup создания новых элементов
-  createPopup.classList.remove('createPopup_active') 
-} 
-function addCard(evt) { // функция добавления новой карточки
+function addCard(evt) { //функция заполнения и сохранения инпутов для карточки
   evt.preventDefault();
   const nameInput = nameCard.value;
   const linkInput = linkCard.value;
@@ -81,42 +75,29 @@ function addCard(evt) { // функция добавления новой кар
   }
   prependCard(dataCard);
   evt.target.reset(); // стираем введенные параметры в input после добавления карточки
-  popupCreateClose();
+  closePopup(createPopup);
 }
  formAdd.addEventListener('submit', addCard)
-
-function bigcard (src, alt) {
-  const cardphoto = document.querySelector('.bigCard__photo').src;
-  const cardtitle = document.querySelector('.bigCard__title').alt;
-  function opencard() { 
-    popup.classList.add('bigCard_active'); 
-  } 
-  const openbig = document.querySelector('.element__photo');
-  openbig.addEventListener('click', opencard);
-  bigcard()
+function openPopup(popup) { //создаем функцию открытия попапов
+  popup.classList.add('popup_active');
+  nameInput.value = nameMain.textContent;
+  jobInput.value = jobMain.textContent;
 }
 
-
-function openPopup() { 
-  popup.classList.add('popup_active') 
-  nameInput.value = nameMain.textContent; 
-  jobInput.value = jobMain.textContent;  
-}  
-function closePopup() { 
-  popup.classList.remove('popup_active') 
-} 
-function submitForm(event) { 
-  event.preventDefault() 
-  nameMain.textContent = nameInput.value; 
-  jobMain.textContent = jobInput.value; 
-  closePopup(); 
+function closePopup(popup) {  //создаем функцию закрытия попапов 
+  popup.classList.remove('popup_active');
 }
 
+function submitForm(event) {  //функция соранения изменений в редакторе (имя/должность)
+  event.preventDefault();
+  nameMain.textContent = nameInput.value;
+  jobMain.textContent = jobInput.value;
+  closePopup(popupEdit);
+}
 
+form.addEventListener('submit', submitForm)
 
-formEdit.addEventListener('submit', submitForm) 
- 
-popupClose.addEventListener('click', closePopup) 
-popupOpen.addEventListener('click', openPopup)
-createPopupOpen.addEventListener('click', popupCreate);
-createPopupClose.addEventListener('click', popupCreateClose);
+popupClose.addEventListener('click', () => closePopup(popupEdit));
+popupOpen.addEventListener('click', () => openPopup(popupEdit));
+createPopupClose.addEventListener('click', () => closePopup(createPopup));
+createPopupOpen.addEventListener('click', () => openPopup(createPopup));
