0fdc453a432b433c353b4cf4b6fbf9555a890515
Skrebnev Maxim <maxim.skrebnev@yandex.ru>
2021-11-02 13:35:18 +0300

    workSix

--- script/validate.js
+++ script/validate.js
@@ -6,17 +6,16 @@ const config = {
   submitButtonSelector: '.popup__btn',
   inactiveButtonClass: 'popup__btn_reset',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
 };
 
-function enableValidation(validationConfig) {
-  const forms = [...document.querySelectorAll(validationConfig.formSelector)]
-  forms.forEach((form) => setFormListener(form, validationConfig))
+function enableValidation(validationConfig) { // функция включения валидации
+  const forms = [...document.querySelectorAll(validationConfig.formSelector)] // ищем все формы
+  forms.forEach((form) => setFormListener(form, validationConfig)) // проходим каждую форму в массиве
 }
-function setFormListener(form, config) {
+function setFormListener(form, config) {  // функция обработки формы
   form.addEventListener('submit', handlerSubmit);
   form.addEventListener('input',  () => setSubmitButtonState(form, config))
-  const inputs = [...form.querySelectorAll(config.inputSelector)]
+  const inputs = [...form.querySelectorAll(config.inputSelector)] // ищем поля формы
 
   inputs.forEach(inputElement => {
   inputElement.addEventListener('input',
@@ -24,32 +23,32 @@ function setFormListener(form, config) {
   })
   setSubmitButtonState(form, config);
 }
-function setSubmitButtonState(form, config) {
+function setSubmitButtonState(form, config) { //функция изменения кнопки, если валидация не пройдена, кнопка не активна и меняет цвет
   const button = form.querySelector(config.submitButtonSelector);
 
   button.disabled = !form.checkValidity();
   button.classList.toggle(config.inactiveButtonClass, !form.checkValidity())
 }
-function handlerSubmit(evt) {
+function handlerSubmit(evt) { // функция сохранения без перезагрузки страницы
   evt.preventDefault()
 }
 
-function handleFieldValidation(input, form, config) {
-  if (!input.validity.valid) {
+function handleFieldValidation(input, form, config) { //функция обработки формы на валидность
+  if (!input.validity.valid) { // пишем условия добавления или скрытия ошибок валидации
   showError(input, form, config)
   } else {
   hideError(input, form, config)
   }
 }
-function showError(input, form, config) {
-  const errorElement = form.querySelector(`#${input.id}-error`);
-  input.classList.add(config.inputErrorClass);
-  errorElement.textContent = input.validationMessage;
+function showError(input, form, config) { //функция показа ошибки валидации
+  const errorElement = form.querySelector(`#${input.id}-error`); //выбираем элемент 
+  input.classList.add(config.inputErrorClass); //добавляем строку класса, делаем видимым
+  errorElement.textContent = input.validationMessage; // добавляем стандартный текст браузерной  ошибки
 }
-function hideError(input, form, config) {
+function hideError(input, form, config) { // функция скрытия ошибки при выполнении условий валидации
   const errorElement = form.querySelector(`#${input.id}-error`);
-  input.classList.remove(config.inputErrorClass);
-  errorElement.textContent = '';
+  input.classList.remove(config.inputErrorClass); //скрываем строку
+  errorElement.textContent = ''; // скрываем стандартный текст ошибки
 }
 
 enableValidation(config)
